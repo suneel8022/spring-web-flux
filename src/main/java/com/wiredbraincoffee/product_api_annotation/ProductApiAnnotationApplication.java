88c94177b009bf61e330265959dbabc06ae5da3e
Suneel <suneelhere8022@gmail.com>
2025-01-18 21:04:37 +0530

    Setting up with embedded mongoDb

---
+++ src/main/java/com/wiredbraincoffee/product_api_annotation/ProductApiAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.wiredbraincoffee.product_api_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiredbraincoffee/product_api_annotation/ProductApiAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiredbraincoffee.product_api_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
