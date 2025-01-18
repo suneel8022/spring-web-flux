80513dc9f26dd95e6d421debf385bfe17d151a93
Suneel <suneelhere8022@gmail.com>
2025-01-18 22:36:34 +0530

    Added Controller Layer with findAll

---
+++ src/main/java/com/wiredbraincoffee/product_api_annotation/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.wiredbraincoffee.product_api_annotation.controller;
+
+
+import com.wiredbraincoffee.product_api_annotation.model.Product;
+import com.wiredbraincoffee.product_api_annotation.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository){
+        this.productRepository=productRepository;
+    }
+
+
+    @GetMapping
+    public Flux<Product> getAllProducts(){
+        // if u observe there is no subscribe -> Spring will call it for you at right time :)
+        return productRepository.findAll();
+    }
+
+}
